43660f270323d3a7b1f6718a6ec180ed3451f1c9
Maciej <maciekqwertyzaqwsx@gmail.com>
2019-11-29 02:06:22 +0100

    created tests

---
+++ src/main/java/RomanNumberConverter.java
@@ -0,0 +1,11 @@
+public class RomanNumberConverter {
+
+    public String convertArabicToRomanNumber(int arabicNumber) {
+        return "";
+    }
+
+    public int convertRomanToArabicNumber(String romanNumber) {
+        return 0;
+    }
+
+}
